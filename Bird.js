8796f5216c6aa31fb2dd04749fe0bcb14b089374
P-Gayathri-Valli <vadivupgl@gmail.com>
2021-02-17 20:00:04 +0530

    vanished

--- Bird.js
+++ Bird.js
@@ -4,9 +4,13 @@ class Bird extends BaseClass {
     this.image = loadImage("sprites/bird.png");
   }
 
-  display() {
+  /*display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
-    super.display();
-  }
+    var angle = this.body.angle;
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();  }*/
 }
